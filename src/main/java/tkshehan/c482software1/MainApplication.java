a5300b021b99a5b70ed2b6fe9beac8c8f345eb45
tkshehan <thomas.shehan88@gmail.com>
2022-11-26 12:12:25 -0500

    Design add_part menu

--- src/main/java/tkshehan/c482software1/MainApplication.java
+++ src/main/java/tkshehan/c482software1/MainApplication.java
@@ -12,7 +12,6 @@ public class MainApplication extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(MainApplication.class.getResource("main.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 900, 480);
-        stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
     }
