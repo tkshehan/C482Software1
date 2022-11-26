799c592b969291151d59096b8dda7b916a112c76
tkshehan <thomas.shehan88@gmail.com>
2022-11-26 10:01:20 -0500

    initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module tkshehan.c482software1 {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens tkshehan.c482software1 to javafx.fxml;
+    exports tkshehan.c482software1;
+}
